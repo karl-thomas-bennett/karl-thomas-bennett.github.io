12f32b64ca279c1c73900fb0c40a9069516e79f7
Karl Bennett <karlthomasbennett@gmail.com>
2021-09-26 20:41:36 +1300

    Add problem solving blog

--- pages/blog/tech/javascript-dom.js
+++ pages/blog/tech/javascript-dom.js
@@ -2,5 +2,9 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
     let header = document.getElementById("header")
-    document.getElementsByTagName('body')[0].addEventListener('click', (evt) => {header.innerText = "(" + evt.pageX + ", " + evt.pageY + ")"})
+    document.getElementsByTagName('body')[0].addEventListener('click', 
+    (evt) => {
+        header.innerText = "(" + evt.pageX + ", " + evt.pageY + ")"; 
+    }
+    )
 }
